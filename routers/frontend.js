a8fd35455b0ae07dce518868d023920f7d6eb538
ajh657 <33629266+ajh657@users.noreply.github.com>
2019-04-18 12:15:45 +0300

    enabled js, css and other site files serving

--- routers/frontend.js
+++ routers/frontend.js
@@ -3,8 +3,43 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/', (req,res) => {
-    var homePath = path.resolve(__dirname + '/../frontend/html/index.html')
-    res.sendFile(homePath);
+    var frontpagePath = path.resolve(__dirname + '/../frontend/html/index.html')
+
+    console.log('Fontpage requested');
+    console.log('Requester ip: ' + req.ip);
+    console.log();
+
+    res.sendFile(htmlPath);
+})
+
+router.get('/:file', (req,res) => {
+    var htmlPath = path.resolve(__dirname + '/../frontend/html/' + req.params.file + ".html")
+
+    console.log('Page: ' + req.params.file +' Requested');
+    console.log('Requester ip: ' + req.ip);
+    console.log();
+
+    res.sendFile(htmlPath);
+})
+
+router.get('/js/:file', (req,res) => {
+    var jsPath = path.resolve(__dirname + '/../frontend/js/' + req.params.file)
+
+    console.log('File: ' + req.params.file +' Requested');
+    console.log('Requester ip: ' + req.ip);
+    console.log();
+
+    res.sendFile(jsPath);
+})
+
+router.get('/css/:file', (req,res) => {
+    var cssPath = path.resolve(__dirname + '/../frontend/css/' + req.params.file)
+
+    console.log('File: ' + req.params.file +' Requested');
+    console.log('Requester ip: ' + req.ip);
+    console.log();
+
+    res.sendFile(cssPath);
 })
 
 module.exports = router;
