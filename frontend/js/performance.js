24eb405cf7ea846fbcaf80374bb90acf6c88b13e
ajh657 <33629266+ajh657@users.noreply.github.com>
2019-04-18 16:07:52 +0300

    pretty

--- frontend/js/performance.js
+++ frontend/js/performance.js
@@ -9,23 +9,20 @@ var opts = {
     },
     limitMax: false,     // If false, max value increases automatically if value > maxValue
     limitMin: false,     // If true, the min value of the gauge will be fixed
-    colorStart: '#6FADCF',   // Colors
-    colorStop: '#8FC0DA',    // just experiment with them
-    strokeColor: '#E0E0E0',  // to see which ones work best for you
-    generateGradient: true,
+    //colorStart: '#6FADCF',   // Colors
+    //colorStop: '#8FC0DA',    // just experiment with them
+    //strokeColor: '#E0E0E0',  // to see which ones work best for you
+    //generateGradient: true,
     highDpiSupport: true,     // High resolution support
-    staticZones: [
-        {strokeStyle: "#00FF00", min: 0, max: 60}, // Red from 100 to 60
-        {strokeStyle: "#0000FF", min: 60, max: 150}, // Yellow
-        {strokeStyle: "#00FFFF", min: 150, max: 220}, // Green
-        {strokeStyle: "#FFDD00", min: 220, max: 260}, // Yellow
-        {strokeStyle: "#FF0000", min: 260, max: 300}  // Red
-    ],
+    percentColors = [[0.0, "00ff00" ], [0.75, "#ffff00"], [1.0, "#ff0000"]],
     
 };
+
 var target = document.getElementById('cpu'); // your canvas element
 var gauge = new Gauge(target).setOptions(opts); // create sexy gauge!
-gauge.maxValue = 300; // set max gauge value
+
+gauge.maxValue = 100; // set max gauge value
+
 gauge.setMinValue(0);  // Prefer setter over gauge.minValue = 0
-gauge.animationSpeed = 50; // set animation speed (32 is default value)
-gauge.set(20); // set actual value
+gauge.animationSpeed = 1; // set animation speed (32 is default value)
+gauge.set(0); // set actual value
