09c01ca5c80db99143ca887266514b4794d87a7c
ajh657 <33629266+ajh657@users.noreply.github.com>
2019-04-18 10:58:25 +0300

    added data store and testing github webhook

--- routers/git.js
+++ routers/git.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const git = require('simple-git');
+const Store = require('data-store')
+const data = new Store({ path: './../data/git.json'});
+
 var router = express.Router();
 
 router.get('/', (req, res) => {
   res.send('Access Denied')
 })
+
+router.post('/', (req,res) => {
+    data.Store('test', JSON.stringify(req.body))
+});
 
 module.exports = router;
