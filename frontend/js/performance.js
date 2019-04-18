8a7f0f42c90245efbb3ca355ac4423936ba5d1e0
ajh657 <33629266+ajh657@users.noreply.github.com>
2019-04-18 14:58:01 +0300

    name fix

--- frontend/js/performance.js
+++ frontend/js/performance.js
@@ -1,9 +1,9 @@
 var cpuKnob = pureknob.createKnob(300, 300);
 
 cpuKnob.setProperty('cpu', 0);
-myKnob.setValue(70);
-myKnob.setPeaks([100]);
+cpuKnob.setValue(70);
+cpuKnob.setPeaks([100]);
 
-var node = knob.node();
+var node = cpuKnob.node();
 var elem = document.getElementById('cpuKnob');
 elem.appendChild(node);
