db432ce4bb4687d594b0d5f39d79fec8820f75b1
ajh657 <33629266+ajh657@users.noreply.github.com>
2019-04-18 17:22:31 +0300

    test

--- frontend/js/performance.js
+++ frontend/js/performance.js
@@ -1,10 +1,16 @@
-var radialObj = radialIndicator('#cpu', {
+var radialObjold = radialIndicator('#cpu', {
     barColor : '#87CEEB',
     barWidth : 10,
     initValue : 40,
     maxValue : 100,
     precentage : true,
     roundCorner : true
+}); 
+
+var radialObj = radialIndicator('#cpu', {
+    barColor : '#87CEEB',
+    barWidth : 10,
+    initValue : 40
 }); 
  
 //Using Instance
