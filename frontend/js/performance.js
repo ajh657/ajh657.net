6b6ce9a80d91e0d5acf3edccd4332d70a4710177
ajh657 <33629266+ajh657@users.noreply.github.com>
2019-04-18 14:58:42 +0300

    commenting

--- frontend/js/performance.js
+++ frontend/js/performance.js
@@ -2,7 +2,7 @@ var cpuKnob = pureknob.createKnob(300, 300);
 
 cpuKnob.setProperty('cpu', 0);
 cpuKnob.setValue(70);
-cpuKnob.setPeaks([100]);
+//cpuKnob.setPeaks([100]);
 
 var node = cpuKnob.node();
 var elem = document.getElementById('cpuKnob');
