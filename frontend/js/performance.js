829e19f3020a900f2de605d686f50526639574a7
ajh657 <33629266+ajh657@users.noreply.github.com>
2019-04-18 17:14:35 +0300

    another one

--- frontend/js/performance.js
+++ frontend/js/performance.js
@@ -1,34 +1,11 @@
-var gauge = new RadialGauge({
-    renderTo: 'cpu',
-    width: 300,
-    height: 300,
-    units: "CPU",
-    minValue: 0,
-    startAngle: 90,
-    ticksAngle: 180,
-    valueBox: false,
-    maxValue: 100,
-    majorTicks: [
-        "0",
-        "100"
-    ],
-    minorTicks: 2,
-    strokeTicks: true,
-    highlights: [
-        {
-            "from": 160,
-            "to": 220,
-            "color": "rgba(200, 50, 50, .75)"
-        }
-    ],
-    colorPlate: "#fff",
-    borderShadowWidth: 0,
-    borders: false,
-    needleType: "arrow",
-    needleWidth: 2,
-    needleCircleSize: 7,
-    needleCircleOuter: true,
-    needleCircleInner: false,
-    animationDuration: 1500,
-    animationRule: "linear"
-}).draw();
+var radialObj = radialIndicator('#indicatorContainer', {
+    barColor : '#87CEEB',
+    barWidth : 10,
+    initValue : 40,
+    maxValue : 100,
+    precentage : true,
+    roundCorner : true
+}); 
+ 
+//Using Instance
+radialObj.animate(1); 
