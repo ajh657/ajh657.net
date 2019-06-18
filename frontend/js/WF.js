a74d120bbb8fa7cfeb7d4eb01a692032ce2c8bd9
ajh657 <33629266+ajh657@users.noreply.github.com>
2019-06-19 00:32:00 +0300

    wf panel start

---
+++ frontend/js/WF.js
@@ -0,0 +1,22 @@
+
+function init() {
+    updateNews();
+}
+
+async function updateNews () {
+    var news = JSON.parse(request("news"))
+    console.log(news)
+}
+
+function request(type) {
+    const Http = new XMLHttpRequest();
+    const url='https://api.ajh657.net/wf/' + type;
+    Http.open("POST", url);
+    Http.send();
+
+    Http.onreadystatechange = (e) => {
+      if (this.readyState == 4) {
+        return Http.responseText;
+      }
+    }
+}
