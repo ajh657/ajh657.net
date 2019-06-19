aa18519383611c947d5646d90ed79b30def2c822
ajh657 <33629266+ajh657@users.noreply.github.com>
2019-06-19 14:34:45 +0300

    news creation

--- frontend/js/WF.js
+++ frontend/js/WF.js
@@ -8,7 +8,10 @@ function init() {
 }
 
 async function updateNews(data) {
-  console.log(data)
+  for (let i = 0; i < data.length; i++) {
+    const element = array[i];
+    createBox(element.message, element.message, element.id, "News")
+  }
 }
 
 function httpGetAsync(theUrl, cFunction)
@@ -16,10 +19,28 @@ function httpGetAsync(theUrl, cFunction)
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.onreadystatechange = function() { 
     if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-      cFunction(xmlHttp.responseText);
+      cFunction(JSON.parse(xmlHttp.responseText));
     }
   }
 
   xmlHttp.open("GET", theUrl, true); // true for asynchronous 
   xmlHttp.send(null);
+}
+
+function createBox(Title ,text, id , rootID) {
+  var box = document.createElement('div');
+  box.className += "panel panel-default"
+  box.id = id;
+
+  var boxHeading = document.createElement('div');
+  box.className += "panel-heading"
+  box.id = id + "Heading";
+
+  var boxContent = document.createElement('div');
+  box.className += "panel-body"
+  box.id = id + "Content";
+
+  document.getElementById(rootID).appendChild(box);
+  document.getElementById(box.id).appendChild(boxHeading);
+  document.getElementById(box.id).appendChild(boxContent);
 }
