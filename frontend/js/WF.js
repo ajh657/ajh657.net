0a5933925731d1a94da80b52bc49b13bd028a2d0
ajh657 <33629266+ajh657@users.noreply.github.com>
2019-06-19 14:06:20 +0300

    test

--- frontend/js/WF.js
+++ frontend/js/WF.js
@@ -11,13 +11,15 @@ async function updateNews(data) {
   console.log(data)
 }
 
-function httpGetAsync(theUrl, callback)
+function httpGetAsync(theUrl, cFunction)
 {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() { 
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+      cFunction(xmlHttp.responseText);
     }
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
-    xmlHttp.send(null);
+  }
+
+  xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+  xmlHttp.send(null);
 }
