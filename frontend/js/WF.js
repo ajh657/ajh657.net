02bc86752eb09eef890696ff81af9a461aa47463
ajh657 <33629266+ajh657@users.noreply.github.com>
2019-06-19 13:31:08 +0300

    test

--- frontend/js/WF.js
+++ frontend/js/WF.js
@@ -1,3 +1,4 @@
+import { url } from "inspector";
 
 function init() {
     updateNews();
@@ -14,15 +15,10 @@ function updateNews () {
 
 }
 
-function request(type) {
-    const Http = new XMLHttpRequest();
-    const url='http://api.ajh657.net/wf/' + type;
-    Http.open("GET", url, false);
-    Http.send();
-
-    Http.onreadystatechange = (e) => {
-      if (this.readyState == 4) {
-        return Http.responseText;
-      }
-    }
+function request(url) {
+  fetch(url).then(function(data) {
+    return data;
+  }).catch(function(error) {
+    console.log(error)
+  })
 }
